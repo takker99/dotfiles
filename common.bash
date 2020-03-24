95143b2b7c9870958f2c9fcb5a7dc1bf864e1805
takker99 <electronic.homuhomu@gmail.com>
2020-03-24 14:24:30 +0900

    :sparkles: 起動時の shell を fish に変更
    - bash を経由せずに fish を直接起動するように変更した
    - bash と fishとで共通して使う設定ファイル `common.bash`を追加
    - fish専用の設定を `common.fish`に移動し git の管理下に置いた
    - fish の vi mode を有効にした
    - fish plugins の list `fishfile` を git に置いた

---
+++ common.bash
@@ -0,0 +1,25 @@
+# bash及びfish共通設定
+
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias vim=nvim
+export EDITOR=vim
+export GIT_EDITOR=vim
+export CC=clang
+export CXX=clang++
+export XDG_CONFIG_HOME=$HOME/.config
+export PATH="$HOME/.local/bin:$PATH"
+export LD_LIBRARY_PATH=$HOME/.local/lib/:$LD_LIBRARY_PATH
+export DISPLAY=:0.0
+export PATH=$HOME/.nodebrew/current/bin:$PATH
+
+# gitをhubに置き換える
+eval "$(hub alias -s)"
+
+# pyenvのpath設定
+export PYENV_ROOT=$HOME/.pyenv
+export PATH=$PYENV_ROOT/bin:$PATH
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
---
+++ common.fish
@@ -0,0 +1,6 @@
+fish_vi_key_bindings
+
+# 共通 shell settings を読み込む
+if test -f $HOME/git/dotfiles/common.bash
+    bass source $HOME/git/dotfiles/common.bash
+end
