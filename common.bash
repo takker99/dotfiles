19f4f315dd799bddd23327ce4da4bbc915af9974
takker99 <electronic.homuhomu@gmail.com>
2020-06-06 00:56:25 +0900

    :sparkles: Add  yarn apt repository automatically if needed

--- common.bash
+++ common.bash
@@ -18,6 +18,18 @@ export PATH=$HOME/.cargo/bin:$PATH
 export BAT_THEME=OneHalfLight
 alias cat=batcat
 
+# yarnのinstall
+if !(type yarn > /dev/null 2>&1); then
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+    sudo apt update
+#    if type nvm > /dev/null 2>&1); then
+#	sudo apt install --no-install-recommends yarn
+#    elif
+#	sudo apt install yarn
+#    fi
+fi
+
 # dotnetをwindows環境のものに置き換える
 alias dotnet='dotnet.exe'
 
