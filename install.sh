7ee6335af23d0ce06817eef3768b457b5263f75b
takker <37929109+takker99@users.noreply.github.com>
2021-10-06 13:44:26 +0900

    :sparkles: Backup the original `.bashrc`

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-mv ~/.bashrc ~/.bashrc~
-curl https://raw.githubusercontent.com/takker99/dotfiles/auto-install/setup.bash > ~/.bashrc
+curl https://raw.githubusercontent.com/takker99/dotfiles/auto-install/setup.bash > ~/.setup.bash
+ln -sb ~/.setup.bash ~/.bashrc
 . ~/.bashrc
