383e9318a5995498b7f7a6d84b8957ded4ddf663
takker99 <37929109+takker99@users.noreply.github.com>
2021-10-09 07:20:06 +0900

    :bug: Wrong filename

--- setup_bashrc.sh
+++ setup_bashrc.sh
@@ -10,8 +10,8 @@ cat << EOS >> ~/.bashrc
 if [ -e ~/git/dotfiles/variables.sh ]; then
   . ~/git/dotfiles/variables.sh
 fi
-if [ -e ~/git/dotfiles/auto_install.sh ]; then
-  bash ~/git/dotfiles/auto_install.sh
+if [ -e ~/git/dotfiles/install_apps.sh ]; then
+  bash ~/git/dotfiles/install_apps.sh
 fi
 if [ -e ~/git/dotfiles/alias.sh ]; then
   . ~/git/dotfiles/alias.sh
