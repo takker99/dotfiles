8cbcaa5559df4d0bacbb612e5ee2059108cb6391
takker99 <37929109+takker99@users.noreply.github.com>
2021-10-08 18:24:43 +0900

    :sparkles: Use XWindow on WSL2

--- variables.sh
+++ variables.sh
@@ -4,3 +4,4 @@ export PATH="$DENO_INSTALL/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/.npm-global/bin:$PATH"
 export XDG_CONFIG_HOME="$HOME/.config"
+export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
