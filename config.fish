b30393cfd5a2cbb9857f5ca696498ec0830879d3
takker99 <37929109+takker99@users.noreply.github.com>
2021-10-09 07:51:56 +0900

    :sparkles: Use abbrs in fish

--- config.fish
+++ config.fish
@@ -7,4 +7,14 @@ if status is-interactive
     ln -sb ~/git/dotfiles/fish_plugins $__fish_config_dir/fish_plugins
     fisher
   end
+
+  # abbrs
+  if type exa > /dev/null 2>&1
+    abbr -ag ls 'exa --icons --color-scale --git'
+    abbr -ag la 'exa --icons --color-scale --git -la'
+  end
+  if type batcat > /dev/null 2>&1
+    abbr -ag cat 'batcat --theme=ansi-dark --paging=never'
+  end
+
 end
