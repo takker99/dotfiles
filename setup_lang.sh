2fdec393dc42d0ab576e5cbb4f24f5e289fe2561
takker <37929109+takker99@users.noreply.github.com>
2023-08-31 11:43:56 +0900

    :bug: failed to restart WSL

--- setup_lang.sh
+++ setup_lang.sh
@@ -1,7 +1,6 @@
 sudo sed -i.bak -e "s/http:\/\/archive\.ubuntu\.com/http:\/\/jp\.archive\.ubuntu\.com/g" /etc/apt/sources.list
 sudo apt update
-sudo apt install language-pack-ja-base -y
+sudo apt -y install language-pack-ja-base
 sudo update-locale LANG=ja_JP.UTF8
-sudo restart
-sudo dpkg-reconfigure tzdata
+cp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 sudo apt -y install manpages-ja manpages-ja-dev
